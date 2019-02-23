e489b88f4d1c380a3670b9ae5d8c999316cf6b79
Tobija Žuntar <tobija.zuntar@gmail.com>
2019-02-23 11:24:42 +0100

    Removed a comment

--- Controller.java
+++ Controller.java
@@ -352,8 +352,6 @@ public class Controller extends Component {
         if (symbolCode != null) {
             appendHTMLText(textEdit, symbolCode);
         }
-
-        // <p>I will display &euro;</p>
     }
 
     /* PRINTING */
