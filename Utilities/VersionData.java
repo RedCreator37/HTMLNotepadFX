bd3de17307db47003fefca29f053483a3fea05bf
RedCreator37 <tobija.zuntar@gmail.com>
2019-04-07 10:38:27 +0200

    Fix "floating" menus, update VersionData.java

--- Utilities/VersionData.java
+++ Utilities/VersionData.java
@@ -29,9 +29,9 @@ public class VersionData {
 
     public static final String VERSION = "0.3 HTML";
 
-    public static final int BUILD_NUMBER = 1054;
+    public static final int BUILD_NUMBER = 1057;
 
-    public static final String BUILD_DATE = "February 2019";
+    public static final String BUILD_DATE = "April 2019";
 
     public static final boolean IS_BETA = true;
 }
