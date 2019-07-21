a4bdc1210330f1298b0fdb6916fbe1a3d03fe18b
RedCreator37 <tobija.zuntar@gmail.com>
2019-07-21 11:42:33 +0200

    The start method will now attempt to make the window small enough to fit on small screens

--- MainFX.java
+++ MainFX.java
@@ -4,6 +4,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.util.Pair;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
 
 /**
  * Main application class for Notepad
@@ -16,7 +20,14 @@ public class MainFX extends Application {
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("FXML/MainWindow.fxml"));
         primaryStage.setTitle("Untitled - Notepad");
-        primaryStage.setScene(new Scene(root, 1135, 700));
+
+        // adjust the window size to make it work on smaller screens
+        Pair<Double, Double> screenSize = getScreenSize();
+        double width = 1135, height = 700;
+        if (screenSize.getKey() < height) height = screenSize.getKey();
+        if (screenSize.getValue() < width) width = screenSize.getValue();
+
+        primaryStage.setScene(new Scene(root, width, height));
         primaryStage.show();
 
         currentStage = primaryStage;
@@ -56,6 +67,17 @@ public class MainFX extends Application {
      */
     static void setTitle(String newTitle, Stage stage) {
         stage.setTitle(newTitle);
+    }
+
+    /**
+     * Get the screen size and return it in favor of a key - value
+     * pair (key is screen wight, value is screen height)
+     */
+    private static Pair<Double, Double> getScreenSize() {
+        Dimension size = Toolkit.getDefaultToolkit().getScreenSize();
+        double width = size.getWidth(), height = size.getHeight();
+
+        return new Pair<>(width, height);
     }
 
 }
