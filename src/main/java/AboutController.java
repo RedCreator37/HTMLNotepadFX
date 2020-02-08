04baae97c02d0aece60f622025faa4186f84f812
RedCreator37 <tobija.zuntar@gmail.com>
2020-02-08 10:35:36 +0100

    Load the license text from resources instead of hardcoding it

--- src/main/java/AboutController.java
+++ src/main/java/AboutController.java
@@ -3,9 +3,11 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
+import util.FileIO;
 import util.VersionData;
 
 import java.io.File;
+import java.util.Objects;
 
 /**
  * Controller class for About.fxml
@@ -34,28 +36,9 @@ public class AboutController {
         diskLabel.setText(new File("/").getTotalSpace() / 1073741824 + " GB");
 
         // display the license
-        String licenseText = "MIT License\n" +
-                "\n" +
-                "Copyright (c) 2019 Tobija Žuntar\n" +
-                "\n" +
-                "Permission is hereby granted, free of charge, to any person obtaining a copy\n" +
-                "of this software and associated documentation files (the \"Software\"), to deal\n" +
-                "in the Software without restriction, including without limitation the rights\n" +
-                "to use, copy, modify, merge, publish, distribute, sublicense, and/or sell\n" +
-                "copies of the Software, and to permit persons to whom the Software is\n" +
-                "furnished to do so, subject to the following conditions:\n" +
-                "\n" +
-                "The above copyright notice and this permission notice shall be included in all\n" +
-                "copies or substantial portions of the Software.\n" +
-                "\n" +
-                "THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n" +
-                "IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n" +
-                "FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\n" +
-                "AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\n" +
-                "LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\n" +
-                "OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\n" +
-                "SOFTWARE.\n" +
-                "\nSource code: www.github.com/RedCreator37/HTMLNotepadFX";
+        String licenseText = FileIO.openFile(
+                new File(Objects.requireNonNull(getClass().getClassLoader()
+                        .getResource("LICENSE")).getFile()));
         licenseTextArea.setText(licenseText);
     }
 
